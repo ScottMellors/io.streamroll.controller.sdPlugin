fb0b0ba1c9e6d9143c41dad612325ea7745a8e15
Scott <scottmellors@gmail.com>
2022-05-24 16:30:39 +0100

    Removed comments

--- dice_pi.js
+++ dice_pi.js
@@ -1,36 +1,3 @@
-/* if ($SD) {
-    $SD.on('connected', function (jsonObj) {
-        uuid = jsonObj['uuid'];
-        if (jsonObj.hasOwnProperty('actionInfo')) {
-            pluginAction = jsonObj.actionInfo['action'];
-        }
-
-        var workingUUID = jsonObj.actionInfo.payload.settings.diceUUID || undefined;
-        if (workingUUID == undefined) {
-            workingUUID = "";
-        }
-
-        //load settings
-        document.getElementById('dice_uuid').value = workingUUID;
-
-        if (pluginAction == "io.streamroll.controller.basic") {
-
-            //Might need to be flex?
-            document.getElementById("dice_roll_item").style.display = "flex";
-
-            //load setting
-            var diceVal = jsonObj.actionInfo.payload.settings.diceValue;
-            if (diceVal == undefined) {
-                diceVal = "3D6";
-            }
-            document.getElementById("dice_roll_value").value = diceVal;
-        } else {
-            //hide it
-            document.getElementById("dice_roll_item").style.display = "none";
-        }
-    });   
-}; */
-
 function sendValueToPlugin(type) {
     var payload = {};
 
